53d2b51bafe82ce938ebef71fda00f8df05edf79
aar-ops <aarondelaguilaa@gmail.com>
2024-11-19 15:35:19 +0100

    Set up Light & Dark Theme , Shadcn, Navbar

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,7 @@
-import React from "react";
-
 const Home = () => {
   return (
     <>
       <h1 className="h1-bold">Welcome to the world</h1>;
-      <h1 className="h1-bold font-space-grotesk">Welcome to the world</h1>;
     </>
   );
 };
